4baf5e59b7a59c8ef874751a368809e4535f6fc8
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2023-09-21 17:40:56 -0300

    theme

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = const Color(0xFFF7F8FC);
+Color primaryColor = const Color(0xFFC2DAF0);
+Color textButton = const Color(0xFF333333);
+Color lightGray = const Color(0xFFA4A6B3);
